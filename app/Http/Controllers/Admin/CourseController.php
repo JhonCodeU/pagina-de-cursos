1b49e345bb4385ddd433a25157aec44e659b1734
Jhon Arcila Castano <jhon.380172793@ucaldas.edu.co>
2021-08-06 20:07:07 -0500

    btn add status

---
+++ app/Http/Controllers/Admin/CourseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::where('status',2)->paginate();
+        return view('admin.courses.index', ['courses' => $courses]);
+    }
+
+}
--- config/adminlte.php
+++ config/adminlte.php
@@ -85,7 +85,7 @@ return [
 
     'layout_topnav' => null,
     'layout_boxed' => null,
-    'layout_fixed_sidebar' => null,
+    'layout_fixed_sidebar' => true,
     'layout_fixed_navbar' => null,
     'layout_fixed_footer' => null,
     'layout_dark_mode' => null,
@@ -266,10 +266,10 @@ return [
             'active' => ['admin/users*'],
             'can'    => 'Leer usuario'
         ],
-        ['header' => 'account_settings'],
+        ['header' => 'Opciones de cursos'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
+            'text' => 'Pendientes de aprobacion',
+            'route'  => 'admin.courses.index',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
